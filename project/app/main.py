beac4e796f6015ac3fff67a944db982e6d33e3a3
Tomasz Janicki <t.janicki@vectra.pl>
2023-06-06 21:21:26 +0200

    Getting Started

---
+++ project/app/main.py
@@ -0,0 +1,13 @@
+from app.config import Settings, get_settings
+from fastapi import Depends, FastAPI
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing
+    }
