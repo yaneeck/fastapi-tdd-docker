2d0b1c544bc7b03b8ff6af048064683f9b7f238c
Tomasz Janicki <t.janicki@vectra.pl>
2023-06-28 15:05:17 +0200

    reformatting code

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -10,7 +10,9 @@ router = APIRouter()
 
 
 @router.post("/", response_model=SummaryResponseSchema, status_code=201)
-async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema:
+async def create_summary(
+    payload: SummaryPayloadSchema
+) -> SummaryResponseSchema:
     summary_id = await crud.post(payload)
 
     response_object = {"id": summary_id, "url": payload.url}
--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -9,7 +9,10 @@ from app.main import create_application
 
 
 def get_settings_override():
-    return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
+    return Settings(
+            testing=1,
+            database_url=os.environ.get("DATABASE_TEST_URL")
+        )
 
 
 @pytest.fixture(scope="module")
